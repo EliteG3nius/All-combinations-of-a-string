990de7e9fdbb4d0eb1c97e766e0e2776a638ce9e
Jerome Michael Andrews <andrewsjerome8@gmail.com>
2020-10-06 23:10:41 +0530

    Add files via upload

---
+++ RandomPasswordGenerator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]{}*;/,._-"
+
+all = lower+upper+numbers+symbols
+length = 16
+
+password = "".join(random.sample(all,length))
+print (password)
+
+
+# In[ ]:
+
+
+
+
